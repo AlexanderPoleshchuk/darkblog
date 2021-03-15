d2936f6164a5d66b1dd7393075d51201f5b53fcb
alexander <poleshchukalexander@gmail.com>
2021-03-15 19:59:17 +0300

    add new config to admin panel

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,8 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 class Tag(models.Model):
@@ -19,21 +21,25 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Статья')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
     author = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликованно')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статья(ю)'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']
