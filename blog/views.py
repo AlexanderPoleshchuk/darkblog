90a786299cb2bb71f37e2627b35bbbe60dd11b8e
Alexander <poleshchukalexander@gmail.com>
2021-03-16 22:34:58 +0300

    add class PostByCategory

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,23 @@ class Home(ListView):
         context['title'] = 'Classic Blog Design'
         return context
 
+
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
+
 def index(request):
     return render(request, 'blog/index.html')
 
